9d805ef0d89cc4ede10ef3a81ef5486fccf7903a
forkroyal <gfabits12@gmail.com>
2025-10-21 14:27:46 +0200

    '600'

--- ft_putptr.c
+++ ft_putptr.c
@@ -1,28 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_putptr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/17 19:22:42 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/21 14:25:58 by fsitter          ###   ########.fr       */
+/*   Created: 2025/10/21 14:21:50 by fsitter           #+#    #+#             */
+/*   Updated: 2025/10/21 14:27:42 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-
-# include "libft/libft.h"
-# include <stdio.h>
-# include <unistd.h>
-
-// int	ft_printf(const char *str, ...);
-int	ft_putchar(int c);
-int	ft_putstr(char *s);
-int	ft_putnbr(int nb);
-int	ft_putuint(unsigned int nb);
-// int	ft_putptr(unsigned long nb);
-int	ft_puthex(unsigned long n, const char format);
-
-#endif
--- ft_putuint.c
+++ ft_putuint.c
@@ -1,28 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_putuint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/17 19:22:42 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/21 14:25:58 by fsitter          ###   ########.fr       */
+/*   Created: 2025/10/21 14:21:55 by fsitter           #+#    #+#             */
+/*   Updated: 2025/10/21 14:27:17 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "printf.h"
 
-# include "libft/libft.h"
-# include <stdio.h>
-# include <unistd.h>
-
-// int	ft_printf(const char *str, ...);
-int	ft_putchar(int c);
-int	ft_putstr(char *s);
-int	ft_putnbr(int nb);
 int	ft_putuint(unsigned int nb);
-// int	ft_putptr(unsigned long nb);
-int	ft_puthex(unsigned long n, const char format);
 
-#endif
+int	ft_putuint(unsigned int nb)
+{
+	int			len;
+	unsigned long	n;
+
+	len = 0;
+	n = nb;
+
+	while (n > 9)
+	{
+		len += ft_putuint(n / 10);
+		n %= 10;
+	}
+	len += ft_putchar(n + 48);
+	
+	return (len);
+}
+
+int	main(void)
+{
+	int	a;
+	int	i;
+	int	j;
+
+	a = 4200;
+	i = ft_putuint(a);
+	printf("\n");
+	printf("%i\n", i);
+	j = printf("%i", a);
+	printf("\n");
+	printf("%i\n", j);
+}
