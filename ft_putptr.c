99e809d82b1f69d04eeb903e7eaecd9aeeb2b9bb
forkroyal <gfabits12@gmail.com>
2025-10-21 14:53:38 +0200

    '600'

--- ft_putptr.c
+++ ft_putptr.c
@@ -6,7 +6,40 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 14:21:50 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/21 14:27:42 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/21 14:53:32 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+
+int	ft_putptr(unsigned long nb);
+
+int	ft_putptr(unsigned long nb)
+{
+	int len;
+	int is_valid;
+	char *base;
+
+	base = "0123456789abcdef";
+	len = 0;
+	len += ft_putstr("0x");
+	if (nb > 15)
+		len += ft_puthex((nb / 16), 'x');
+	is_valid = ft_putchar(base[nb % 16]);
+	if (is_valid == -1)
+		return (is_valid);
+	return (len + is_valid);
+}
+
+// int	main(void)
+// {
+// 	int	number;
+// 	int *nptr = & number;
+
+// 	number = 42000000;
+
+// 	ft_putptr((unsigned long)nptr);
+// 	printf("\n");
+// 	printf("%p\n", nptr);
+
+// }
--- ft_putuint.c
+++ ft_putuint.c
@@ -6,37 +6,38 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 14:21:55 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/21 14:30:32 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/21 14:53:03 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 
 int	ft_putuint(unsigned int nb);
 
 int	ft_putuint(unsigned int nb)
 {
-	int				len;
-	unsigned long	n;
+	int	len;
+	int	is_valid;
 
 	len = 0;
-	n = nb;
-	if (n > 9)
-		len += ft_putuint(n / 10);
-	len += ft_putchar(n % 10 + 48);
-	return (len);
+	if (nb > 9)
+		len += ft_putuint(nb / 10);
+	is_valid = ft_putchar(nb % 10 + 48);
+	if (is_valid == -1)
+		return (is_valid);
+	return (len + is_valid);
 }
 
 // int	main(void)
 // {
-// 	int	a;
+// 	unsigned int	a;
 // 	int	i;
 // 	int	j;
 
 // 	a = 4200;
 // 	i = ft_putuint(a);
 // 	printf("\n");
-// 	printf("%i\n", i);
+// 	printf("%u\n", i);
 // 	j = printf("%i", a);
 // 	printf("\n");
 // 	printf("%i\n", j);
